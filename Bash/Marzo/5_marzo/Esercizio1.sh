8f4773f253540ea55920edf1ffc968a5a78d8670
Tabb04 <mircotaba229@gmail.com>
2025-03-04 09:34:40 +0100

    4 marzo

---
+++ Bash/Marzo/5_marzo/Esercizio1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#lista soltanto con i file della directory passata come argomento
+#usa il comando findo epr listare i file nella directory passata come argomento
+#nel seguente modo: find <directory> -maxdepth 1 -type f
